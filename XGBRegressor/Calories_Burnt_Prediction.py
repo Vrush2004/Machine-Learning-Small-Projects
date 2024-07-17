8ff4bc60dbe62a1b996e744bb81d23856c9f73fe
Vrush2004 <vrushalichaube@gmail.com>
2024-07-17 10:10:09 +0530

    Calories Burnt Prediction

--- XGBRegressor/Calories_Burnt_Prediction.py
+++ XGBRegressor/Calories_Burnt_Prediction.py
@@ -8,10 +8,46 @@ from xgboost import XGBRegressor
 from sklearn import metrics
 
 # Data collection and processing
-calaries = pd.read_csv(r"C:\Users\Dell\Desktop\XGBRegressor\calories.csv")
+calaries = pd.read_csv(r"C:\Users\Dell\Desktop\Machine Learning Small Projects\XGBRegressor\calories.csv")
 print(calaries.head())
-exercise_data = pd.read_csv(r"C:\Users\Dell\Desktop\XGBRegressor\exercise.csv")
+exercise_data = pd.read_csv(r"C:\Users\Dell\Desktop\Machine Learning Small Projects\XGBRegressor\exercise.csv")
 print(exercise_data.head())
 
 #Combining the two dataframes
-calaries_data = pd.concat(exercise_data, )
+calaries_data = pd.concat([exercise_data, calaries['Calories']],axis=1)
+print(calaries_data.head())
+calaries_data.shape
+calaries_data.info()
+
+#Data Analysis
+calaries_data.describe()
+calaries_data['Gender'] = calaries_data['Gender'].replace({'female': 0, 'male': 1})
+
+#Data Visualization
+sns.set()
+#Ploting the gender column in count plot
+sns.countplot(calaries_data['Gender'])
+#Finding the distribution of 'Age' column
+sns.displot(calaries_data['Age'])
+#Finding the distribution of 'Height' column
+sns.displot(calaries_data['Height'])
+#Finding the distribution of 'Weight' column
+sns.displot(calaries_data['Weight'])
+
+#Finding the Correlation in the dataset
+correlation = calaries_data.corr()
+#Constructing a heatmap to understand the correlation
+plt.figure(figsize=(10,10))
+sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Reds')
+
+
+
+
+
+
+
+
+
+
+
+
