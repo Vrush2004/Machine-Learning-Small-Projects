a4237bbd181ef7d712840def7aa531d9215b5416
Vrush2004 <vrushalichaube@gmail.com>
2024-07-17 15:31:02 +0530

    Calories Burnt Prediction

--- XGBRegressor/Calories_Burnt_Prediction.py
+++ XGBRegressor/Calories_Burnt_Prediction.py
@@ -1,5 +1,4 @@
 # Importing the dependencies
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -40,14 +39,26 @@ correlation = calaries_data.corr()
 plt.figure(figsize=(10,10))
 sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Reds')
 
+#Seperating features and target
+X = calaries_data.drop(columns = ['User_ID','Calories'], axis=1)
+Y = calaries_data['Calories']
 
+#Splitting the data into training data and testing data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
 
+#Model Training: XGBoost Regressor
+model = XGBRegressor()
+model.fit(X_train, Y_train)
 
+#Evaluation 
+#Prediction on test data
+test_data_prediction = model.predict(X_test)
+print(test_data_prediction)
 
-
-
-
-
+#Mean Absolute Error
+mea = metrics.mean_absolute_error(Y_test, test_data_prediction)
+print("Mean Absolute Error: ",mea)
 
 
 
