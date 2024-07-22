e294fde8334587ee68a1ab74080e7cb450ba9d08
Vrush2004 <vrushalichaube@gmail.com>
2024-07-22 09:13:38 +0530

    Medical Insurance Cost Prediction

--- Medical Insurance Cost Prediction/Insurance_cost_prediction.py
+++ Medical Insurance Cost Prediction/Insurance_cost_prediction.py
@@ -66,16 +66,43 @@ insurance_dataset.replace({'sex':{'male':0, 'female':1}}, inplace=True)
 insurance_dataset.replace({'smoker':{'yes':0, 'no':1}}, inplace=True)
 insurance_dataset.replace({'region':{'southwest':0, 'southeast':1, 'northwest':2, 'northeast':3}}, inplace=True)
 
+#Splitting the features and target
+X = insurance_dataset.drop(columns='charges', axis=1)
+Y = insurance_dataset['charges']
 
+#Splitting the data into training data and testing data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
 
+#Model Training : Linear Regression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
 
+#Model Evaluation
+#prediction on training data
+training_data_prediction = regressor.predict(X_train)
+#R squared value
+r2_train = metrics.r2_score(Y_train, training_data_prediction)
+print("R squared value : ",r2_train)
 
+#prediction on test data
+test_data_prediction = regressor.predict(X_test)
+#R squared value
+r2_test = metrics.r2_score(Y_test, test_data_prediction)
+print("R squared value : ",r2_test)
 
+#Building a Predictive System
+input_data = (31,1,25,74,0,1,0)
 
+#changing input data to a numpy array 
+input_data_as_numpy_array = np.asarray(input_data)
 
+#reshape the array 
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+prediction = regressor.predict(input_data_reshaped)
+print(prediction)
 
-
-
+print("The insurance cost is USD " , prediction[0])
 
 
 
